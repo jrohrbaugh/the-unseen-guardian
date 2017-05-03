37f1165f75ad562db3485f4ee15feffe6140626d
Josh Rohrbaugh <jrohrbaugh@gmail.com>
2017-05-03 19:42:31 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170503233434) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string   "name"
     t.integer  "strength"
